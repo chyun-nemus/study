a858b54fb4be687fcebf620e6a93fc76b6e3f1f4
chyun <chyun@nemustech.co.kr>
2017-01-27 17:38:51 +0900

    [Scala] euler_001 수정

--- [Scala]Euler/src/euler001.scala
+++ [Scala]Euler/src/euler001.scala
@@ -1,15 +1,33 @@
 /**
   * Created by chyun on 2017. 1. 22..
   */
-object euler001 extends App{
-  val lastNum = 100
-  var sum = 0
-  for (
-    i <-1 to lastNum
-    if (i % 3 == 0) || (i % 5 == 0)
-  ) {
-    sum += i
-    print(i+" ")
+object euler001 extends App {
+  solution01
+  solution02
+  solution2stepbystep
+  
+  def solution01: Unit = {
+    val lastNum = 1000
+    var sum = 0
+    for (
+      i <- 1 until lastNum
+      if (i % 3 == 0) || (i % 5 == 0)
+    ) {
+      sum += i
+    }
+    println(sum)
   }
-  println("\nResult: "+sum)
+
+  def solution02: Unit = {
+    println(0 until 1000 filter(i => i%3==0 || i%5==0) sum)
+  }
+
+  def solution2stepbystep: Unit = {
+    val starttime = java.lang.System.currentTimeMillis()
+    val range = 0 until 1000
+    val indexedSeq = range.filter(i => i%3==0 || i%5==0)
+    val answer = indexedSeq.sum
+    println(answer)
+
+  }
 }
