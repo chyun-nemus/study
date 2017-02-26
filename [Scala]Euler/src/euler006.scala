83469e30bf89431558a225ac9c6a67eb36b5e81a
chyun <chyun@nemustech.co.kr>
2017-02-26 13:38:21 +0900

    [Scala]euler_project 6 수정

--- [Scala]Euler/src/euler006.scala
+++ [Scala]Euler/src/euler006.scala
@@ -9,15 +9,9 @@
 // 따라서 1부터 10까지 자연수에 대해 "합의 제곱"과 "제곱의 합" 의 차이는 3025 - 385 = 2640 이 됩니다.
 // 그러면 1부터 100까지 자연수에 대해 "합의 제곱"과 "제곱의 합"의 차이는 얼마입니까?
 
-object euler006 extends App{
+object euler006 extends App {
   println(Solution01(100))
-
   def Solution01(length: Int): Int = {
-    val array = (1 to length)
-    val pow_sum= array.map(math.pow(_, 2)).sum.toInt
-    val sum_pow = array.sum * array.sum
-    println(pow_sum + " " + sum_pow)
-    sum_pow - pow_sum
+    (math.pow((1 to length).sum, 2) - (1 to length).map(math.pow(_, 2)).sum).toInt
   }
-
 }
